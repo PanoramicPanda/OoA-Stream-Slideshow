e54598b73ed7283574aeb1c710c9756bfeb32db2
PanoramicPanda <uriah.h.brown@gmail.com>
2024-02-13 21:35:25 -0500

    Add all characters

--- src/main.jsx
+++ src/main.jsx
@@ -9,28 +9,24 @@ import scheduleCard from "./assets/cards/schedule.png";
 import socialsCard from "./assets/cards/socials.png";
 import rinnCard from "./assets/cards/character-rinn-8030631.png";
 import truthCard from "./assets/cards/character-truth-9239322.png";
-
-// sched
-// social
-// heroforge
-// rinn
-// truth
-// brocc
-// ragnar
+import broccCard from "./assets/cards/character-brocc-26208332.png";
+import ragnarCard from "./assets/cards/character-ragnar-29000862.png";
 
 const slideshowImages = [
     scheduleCard,
     socialsCard,
     heroforgeCard,
     rinnCard,
-    truthCard
+    truthCard,
+    broccCard,
+    ragnarCard
 ];
 
 const renderDev = () => {
     const renderMode = import.meta.env.VITE_RENDER_MODE;
 
     if (renderMode === 'character') {
-        return <CharacterCard characterId={9239322} characterImage={truthCard} prefetch={true} />
+        return <CharacterCard characterId={8030631} characterImage={rinnCard} prefetch={true} />
     } else if (renderMode === 'slideshow') {
         return <Slideshow images={slideshowImages} intervalSeconds={45} prefetchSeconds={3} />
     }
